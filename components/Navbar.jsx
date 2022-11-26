7699e53ff77969cb2e25928f41c8e652dcb64d07
ronitgandhi <ronitgandhi@gmail.com>
2022-11-26 16:55:55 +0530

    added main bg color

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,35 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+	<motion.nav
+		variants={navVariants}
+		initial="hidden"
+		whileInView="show"
+		className={`${styles.xPaddings} py-8 relative`}
+	>
+		<div className="absolute w-[50%] inset-0 gradient-01" />
+		<div
+			className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+		>
+			<img
+				className="w-[24px] h-[24px] object-contain"
+				src="/search.svg"
+				alt="searchbutton"
+			/>
+			<h2 className="text-white leading-[30px] text-[24px] font-extrabold ">
+				METAVERSUS
+			</h2>
+			<img
+				src="/menu.svg"
+				alt="menubutton"
+				className="w-[24px] h-[24px] object-contain"
+			/>
+		</div>
+	</motion.nav>
 );
 
 export default Navbar;
