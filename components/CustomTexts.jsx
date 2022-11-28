08d8eee269f19a1ac1fe57f0a2ae6d493a9b034c
ronitgandhi <ronitgandhi@gmail.com>
2022-11-28 09:48:41 +0530

    component for typing effect

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -6,15 +6,23 @@ import { textContainer, textVariant2 } from "../utils/motion";
 export const TypingText = ({ title, textStyles }) => (
 	<motion.p
 		variants={textContainer}
-		initial=""
 		className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
 	>
-		{Array.from(title).map((Letter, index) => (
+		{Array.from(title).map((letter, index) => (
 			<motion.span variants={textVariant2} key={index}>
-				{Letter === " " ? "\u00A0" : Letter}
+				{letter === " " ? "\u00A0" : letter}
 			</motion.span>
 		))}
 	</motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+	<motion.h2
+		variants={textVariant2}
+		initial="hidden"
+		whileInView="show"
+		className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+	>
+		{title}
+	</motion.h2>
+);
