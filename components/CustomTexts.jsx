26769c2a06d04c56407789ecec95f726870a84a2
ronitgandhi <ronitgandhi@gmail.com>
2022-11-28 09:34:21 +0530

    added about section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,20 @@
-'use client';
+"use client";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from "framer-motion";
+import { textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+	<motion.p
+		variants={textContainer}
+		initial=""
+		className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+	>
+		{Array.from(title).map((Letter, index) => (
+			<motion.span variants={textVariant2} key={index}>
+				{Letter === " " ? "\u00A0" : Letter}
+			</motion.span>
+		))}
+	</motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
